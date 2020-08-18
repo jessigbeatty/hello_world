4aaf107f6104353d6b1c3177d9f9a4b65ed35e45
jessigbeatty <49537526+jessigbeatty@users.noreply.github.com>
2020-08-17 21:09:59 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import time
 import os
 
 def time_elapsed():
-    """
-    Counts seconds passed since started and stores them in a global variable, t
+    """Counts seconds passed since started and stores them in a global variable, t
     """
     global t
     t = 0
@@ -13,12 +12,8 @@ def time_elapsed():
         t += 1
         time.sleep(1)
 
-def get_time():
-    print(t)
-
 def get_input():
-    """
-    Gets input from user and prints it
+    """Gets input from user and prints it
     """
     running = True
     while running:
@@ -26,14 +21,22 @@ def get_input():
         print(choice)
         print(t)
 
+def get_and_score():
+    running = True
+    while running:
+        global score; score=0
+        choice = input("Guess how many seconds I've been running: ")
+        print(f"You guessed I've been running {choice} seconds??")
+        print(f"Actually, I've been running {t} seconds.\n")
+        
 #define threads
 t1 = threading.Thread(target=time_elapsed)
-t2 = threading.Thread(target=get_input)
+##t2 = threading.Thread(target=get_input)
 
 #start threads
 t1.start()
 ## t2.start()
-get_input()
 
-t1.join()
-t2.join()
+get_and_score()
+
+
